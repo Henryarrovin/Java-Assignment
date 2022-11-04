6d4f6cd42dca3a8cbee418dcafd5fe550df5f62e
Henryarrovin <113096760+Henryarrovin@users.noreply.github.com>
2022-11-04 11:08:45 +0530

    Update Main.java

--- GenericStack/src/Main.java
+++ GenericStack/src/Main.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>(5);
+        Integer n;
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter how much time u want to push elements in the stack:");
+        n=s.nextInt();
+        Stack<Integer> stack = new Stack<>(n);
+        
         System.out.println("Enter element of stack: ");
         for(int i = 0; i < 5; i++) {
             stack.push(new Scanner(System.in).nextInt());
         }
 
         stack.printStack();
-        System.out.println("Peek: " + stack.peek());
+        System.out.println("Top: " + stack.Top());
         System.out.println("Pop: " + stack.pop());
-        System.out.println("Peek: " + stack.peek());
+        System.out.println("Top: " + stack.Top());
         stack.printStack();
     }
 }
